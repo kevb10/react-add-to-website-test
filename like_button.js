9c79968b558580e638dd3d774681acd922091292
Kevin Manase <gasydev@gmail.com>
2022-05-05 15:47:41 -0500

    add

---
+++ like_button.js
@@ -0,0 +1,14 @@
+var e = React.createElement;
+console.log("I AM WORKING");
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  	render() {
+      return 'Hello World!';
+	}
+}
+
+var domContainer = document.querySelector('#like_button_container');
+ReactDOM.hydrate(e(LikeButton), domContainer);
